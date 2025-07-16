0385e4d6fdd49cd8e2600cfff575af218d442baa
manarbek0vv <manarbek0vvb@mail.ru>
2025-07-17 01:10:32 +0500

    fix styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,7 @@
-import { useEffect } from "react";
 import Container from "./components/Container/Container"
 import Wallet from "./components/Wallet/Wallet"
-import { useTelegram } from "./hooks/useTelegram";
 
 function App() {
-  const { showBackButton, onBackClick, close } = useTelegram();
-
-  useEffect(() => {
-    showBackButton();
-    onBackClick(() => {
-      close(); // Закрыть мини-апп
-    });
-  }, []);
 
   return (
     <Container>
