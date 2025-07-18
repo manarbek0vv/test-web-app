25728cb2de2f854d4f32f4c3f17a651449671599
manarbek0vv <manarbek0vvb@mail.ru>
2025-07-18 21:50:24 +0500

    telegram

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+import { useEffect } from "react"
 import Container from "./components/Container/Container"
 import Navigation from "./components/Navigation/Navigation"
 import Wallet from "./components/Wallet/Wallet"
 
 function App() {
+
+  useEffect(() => {
+    // @ts-ignore
+    const telegram = window.Telegram.WebApp;
+    telegram.requestFullscreen();
+  }, [])
 
   return (
     <Container>
