d014adbc7f7c420767d829d18b7c694e7db96c9e
manarbek0vv <manarbek0vvb@mail.ru>
2025-07-19 21:29:41 +0500

    device orientation

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,15 @@ import Navigation from "./components/Navigation/Navigation"
 import Wallet from "./components/Wallet/Wallet"
 
 function App() {
+  const desktops = ['macos', 'tdesktop', 'weba', 'webk', 'web']
 
   useEffect(() => {
-    Telegram.WebApp.requestFullscreen();
-    Telegram.WebApp.disableVerticalSwipes();
-    Telegram.WebApp.lockOrientation();
+    const tg = Telegram.WebApp;
+    if (!(desktops.includes(tg.platform))) Telegram.WebApp.requestFullscreen();
+    tg.setBackgroundColor('#181818');
+    tg.expand();
+    tg.disableVerticalSwipes();
+    tg.lockOrientation();
   }, [])
 
   return (
